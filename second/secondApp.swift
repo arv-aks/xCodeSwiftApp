65797d6aa44a91580679dbd2e3fdf92e42adec85
@aks.arvind <aks.arvind01@gmail.com>
2024-04-10 09:14:52 +0530

    Initial Commit

---
+++ second/secondApp.swift
@@ -0,0 +1,17 @@
+//
+//  secondApp.swift
+//  second
+//
+//  Created by Arvind on 10/04/24.
+//
+
+import SwiftUI
+
+@main
+struct secondApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
